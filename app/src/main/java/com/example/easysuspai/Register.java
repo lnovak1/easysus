96b866c4c169479971f9ff8ccc8f88bd34ee8e8f
lnovak1 <leonovak98@outlook.com>
2020-06-13 15:28:21 -0300

    spinner bairro/municipio

--- app/src/main/java/com/example/easysuspai/Register.java
+++ app/src/main/java/com/example/easysuspai/Register.java
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
+
 
 public class Register extends AppCompatActivity {
     private Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         button1=(Button)findViewById(R.id.register);
+        Spinner spinnermun = (Spinner) findViewById(R.id.spinnermunicipio);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.municipio, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnermun.setAdapter(adapter);
+
+        Spinner spinnerbairro = (Spinner) findViewById(R.id.spinnerbairro);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.bairro, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerbairro.setAdapter(adapter2);
+
         button1.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
