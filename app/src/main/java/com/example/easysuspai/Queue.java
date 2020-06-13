1fc1b2c6b45c582eed9bb978d7f0de4b68bb6017
lnovak1 <leonovak98@outlook.com>
2020-06-13 20:26:27 -0300

    Relogio;  concatencação de diversos comandos para inject do bd;

--- app/src/main/java/com/example/easysuspai/Queue.java
+++ app/src/main/java/com/example/easysuspai/Queue.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.widget.*;
 import android.view.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+
 
 
 public class Queue extends AppCompatActivity {
@@ -14,8 +20,30 @@ public class Queue extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
+
+
+
+       int b=3;//valor ilusorio de N fila inserir numero da database aqui
+
+
+        TextView timequeue = (TextView) findViewById(R.id.timequeue);
+        java.util.GregorianCalendar gc = new java.util.GregorianCalendar();
+        gc.setTime(new java.util.Date());
+
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("HH:mm");
+        gc.add(java.util.Calendar.MINUTE,15*b);
+        timequeue.setText(sdf.format(gc.getTime()));
+
+
+
+
+        TextView filaposicao = (TextView) findViewById(R.id.filaposicao);
         Button buttonVoltar=(Button)findViewById(R.id.bt_voltar_queue);
         Button sair=(Button)findViewById(R.id.bt_sair_queue);
+        String texto1 = filaposicao.getText().toString();
+        String texto2 = getString(R.string.activity_queue_position2);
+        filaposicao.setText(texto1.toString() +" "+ b +" "+ texto2.toString());
+
         sair.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view){
--- app/src/main/java/com/example/easysuspai/Userscreen.java
+++ app/src/main/java/com/example/easysuspai/Userscreen.java
@@ -15,28 +15,39 @@ public class Userscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userscreen);
+        TextView welcome=(TextView)findViewById(R.id.welcome) ;
         Button buttonMarcarConsulta=(Button)findViewById(R.id.bt_marcar_consulta);
         Button sairuser=(Button)findViewById(R.id.bt_sair);
-        boolean checkfila=false;
+        String nome ="Cremenoso!";//trocar nome pelo da database OBS.: so o primeiro nome
+        String welcome1=getString(R.string.bem_vindo);
+        welcome.setText(welcome1.toString()+nome.toString());
+        final boolean checkfila=false;
         if(checkfila) {
             buttonMarcarConsulta.setText(R.string.bt_checarfila);
-            Intent intent = new Intent(getBaseContext(), Queue.class);
-            startActivity(intent);
+
         }
         else{
             buttonMarcarConsulta.setText(R.string.bt_entrar_fila);
-            //injeçao no DB
-            Intent intent = new Intent(getBaseContext(), Queue.class);
-            startActivity(intent);
+
         }
 
         buttonMarcarConsulta.setOnClickListener(new Button.OnClickListener(){
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
+                if (checkfila) {
 
-                Intent intent = new Intent(getBaseContext(), Queue.class);
-                startActivity(intent);
-            }
+
+                    Intent intent = new Intent(getBaseContext(), Queue.class);
+                    startActivity(intent);
+                }
+                if (!checkfila) {
+                    //inserçaodatabase
+                    Intent intent = new Intent(getBaseContext(), Queue.class);
+                    startActivity(intent);
+                }
+
+                }
+
         });
         sairuser.setOnClickListener(new Button.OnClickListener(){
             @Override
